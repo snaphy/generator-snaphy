893822304175d77082950d01f3e4d025a51c3b6a
Robins Gupta <robinskumar73@gmail.com>
2015-11-30 19:53:08 +0530

    Plugin depedency added

--- generators/app/templates/common/plugins/dashboard/backend/index.js
+++ generators/app/templates/common/plugins/dashboard/backend/index.js
@@ -1,10 +1,22 @@
 'use strict';
-module.exports = function( server, databaseObj, helper) {
-/**
- * Here server is the main app object
- * databaseObj is the mapped database from the package.json file
- * helper object contains all the helpers methods.
- */
+module.exports = function( server, databaseObj, helper, packageObj) {
+	/**
+	 * Here server is the main app object
+	 * databaseObj is the mapped database from the package.json file
+	 * helper object contains all the helpers methods.
+	 */
+
+	/**
+	 * Initialize the plugin at time of server start.
+	 * @return {[type]} [description]
+	 */
+	var init = function(){
+
+	};
 
 
-}
+	//return all the methods that you wish to provide user to extend this plugin.
+	return {
+		init: init
+	}
+} //module.exports
