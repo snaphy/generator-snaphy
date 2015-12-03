2cd195dd0dda4dab7057e9d4ecf1db0f9d998ee0
robinskumar73 <robinskumar73@gmail.com>
2015-12-04 04:44:54 +0530

    Dashboard updated

--- generators/app/templates/common/plugins/dashboard/client/scripts/controllers/dashboard.js
+++ generators/app/templates/common/plugins/dashboard/client/scripts/controllers/dashboard.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module($snaphy.getModuleName())
-
 //Controller for dashboardControl ..
-.controller('dashboardControl', ['$scope', '$stateParams','Database', 
+.controller('dashboardControl', ['$scope', '$stateParams','Database',
     function($scope, $stateParams, Database) {
         //Set snaphy default template value to true..
-        $snaphy.getDefaultTemplate(true);
+        $snaphy.setDefaultTemplate(true);
+        //Adding the login state from the login plugins..
+        $scope.loginState  = $snaphy.loadSettings('login', "loginState");
+        $scope.homeState   = $snaphy.loadSettings('dashboard', "homeState");
     }//controller function..
 ]);
--- generators/app/templates/common/plugins/dashboard/client/scripts/routes/dashboard.js
+++ generators/app/templates/common/plugins/dashboard/client/scripts/routes/dashboard.js
@@ -3,16 +3,27 @@
 /*global $, jQuery, $snaphy, angular*/
 angular.module($snaphy.getModuleName())
   //Routes are defined using ui.routes 
-  .config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
-    function ($locationProvider, $stateProvider, $urlRouterProvider) {
+  .config(['$locationProvider', '$stateProvider',
+    function ($locationProvider, $stateProvider) {
       $locationProvider.html5Mode(false);
+        //Adding the login state from the login plugins..
+        var loginState    = $snaphy.loadSettings('login', "loginState"),
+        homeState         = $snaphy.loadSettings('dashboard', "homeState"),
+        adminRole         = $snaphy.loadSettings('login', "adminRole"),
+        employeeRole      = $snaphy.loadSettings('login', "employeeRole");
 
-      $stateProvider
+        $stateProvider
         //Provide routes in this way..
-        .state('dashboard', {
+        .state(homeState, {
           url: '/',
           templateUrl: '/dashboard/views/dashboard.html',
-          controller: 'dashboardControl'
+          controller: 'dashboardControl',
+            data: {
+                permissions: {
+                    only: [employeeRole],
+                    redirectTo: loginState
+                }
+            }
         });
 
     }]); //config
